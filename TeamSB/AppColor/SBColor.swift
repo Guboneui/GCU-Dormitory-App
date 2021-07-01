5d19abca80b77e5ebb2c85d23c121ad14a85aab7
Guboneui <starku2249@naver.com>
2021-07-01 22:21:59 +0900

    앱 컬러 파일 추가

---
+++ TeamSB/AppColor/SBColor.swift
@@ -0,0 +1,18 @@
+//
+//  SBColor.swift
+//  TeamSB
+//
+//  Created by 구본의 on 2021/07/01.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    struct SBColor {
+        static var SB_LightYellow = UIColor(red: 253/255, green: 242/255, blue: 208/255, alpha: 1)
+        static var SB_BaseYellow = UIColor(red: 249/255, green: 218/255, blue: 120/255, alpha: 1)
+        static var SB_DarkGray = UIColor(red: 174/255, green: 171/255, blue: 171/255, alpha: 1)
+        static var SB_LightGray = UIColor(red: 191/255, green: 191/255, blue: 191/255, alpha: 1)
+    }
+}
