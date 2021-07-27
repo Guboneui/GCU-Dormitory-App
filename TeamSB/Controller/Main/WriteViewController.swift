dd4148cbe08ce1783357beb16e592e442a2b9ef8
Guboneui <starku2249@naver.com>
2021-07-28 02:40:18 +0900

    글쓰기 빈 태그값 오류 해결

--- TeamSB/Controller/Main/WriteViewController.swift
+++ TeamSB/Controller/Main/WriteViewController.swift
@@ -129,7 +129,13 @@ class WriteViewController: UIViewController {
             self.present(alert, animated: true, completion: nil)
             
         } else {
-            postWriteArticle()
+            
+            if tagArray.count == 0 {
+                postWriteArticleNoHash()
+            } else {
+                postWriteArticle()
+            }
+
         }
     }
     
@@ -180,6 +186,74 @@ class WriteViewController: UIViewController {
             "userId": UserDefaults.standard.string(forKey: "userID")!,
             "text": contentsTextView.text!,
             "hash": tagArray,
+        ]
+        
+        let alamo = AF.request(URL, method: .post, parameters: PARAM).validate(statusCode: 200...500)
+        
+        alamo.responseJSON{(response) in
+            
+            switch response.result {
+            case .success(let value):
+                if let jsonObj = value as? NSDictionary {
+                    print(">> \(URL)")
+                    print(">> 게시글 작성 API 호출 성공")
+                    
+                    let result = jsonObj.object(forKey: "check") as! Bool
+                    if result == true {
+                        let message = jsonObj.object(forKey: "message") as! String
+                        print(">> \(message)")
+                        
+                        let alert = UIAlertController(title: message, message: "", preferredStyle: .alert)
+                        let okButton = UIAlertAction(title: "확인", style: .default, handler: {[self] _ in
+                            
+                            delegate?.update()
+                            self.navigationController?.popViewController(animated: true)
+                        })
+                        alert.addAction(okButton)
+                        self.present(alert, animated: true, completion: nil)
+                        
+                    } else {
+                        let message = jsonObj.object(forKey: "message") as! String
+                        
+                        let alert = UIAlertController(title: message, message: "", preferredStyle: .alert)
+                        let okButton = UIAlertAction(title: "확인", style: .default, handler: nil)
+                        alert.addAction(okButton)
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                    
+                }
+                
+            case .failure(let error) :
+                if let jsonObj = error as? NSDictionary {
+                    print("서버통신 실패")
+                    print(jsonObj)
+                }
+            }
+        }
+    }
+    
+    
+    
+    func postWriteArticleNoHash() {
+        let URL = "http://13.209.10.30:3000/writeArticle"
+        
+        var category = ""
+        if categoryTitle.text == "배달" {
+            category = "delivery"
+        } else if categoryTitle.text == "택배" {
+            category = "parcel"
+        } else if categoryTitle.text == "택시" {
+            category = "taxi"
+        } else if categoryTitle.text == "빨래" {
+            category = "laundry"
+        }
+       
+        
+        let PARAM: Parameters = [
+            "title": titleTextField.text!,
+            "category": category,
+            "userId": UserDefaults.standard.string(forKey: "userID")!,
+            "text": contentsTextView.text!,
         ]
         
         let alamo = AF.request(URL, method: .post, parameters: PARAM).validate(statusCode: 200...500)
