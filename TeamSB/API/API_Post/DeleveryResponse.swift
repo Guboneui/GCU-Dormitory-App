bf0a524bcc093f5bc5406256047960282cf8ad09
Guboneui <starku2249@naver.com>
2021-07-30 04:39:14 +0900

    배달 카테고리 코드 리펙토링

---
+++ TeamSB/API/API_Post/DeleveryResponse.swift
@@ -0,0 +1,26 @@
+//  DeleveryResponse.swift
+//  TeamSB
+//  Created by 구본의 on 2021/07/30.
+
+import Foundation
+
+struct DeleveryResponse: Decodable {
+    var check: Bool
+    var code: Int
+    var message: String
+    var content: [Delevery]?
+}
+
+struct Delevery: Decodable {
+    var no: Int
+    var title: String
+    var category: String
+    var timeStamp: String
+    var mod_timeStamp: String
+    var userId: String
+    var userNickname: String
+    var text: String
+    var viewCount: Int
+    var reportCount: Int
+    var hash: [String]
+}
--- TeamSB/Controller/Post/Delivery/DeleveryViewController.swift
+++ TeamSB/Controller/Post/Delivery/DeleveryViewController.swift
@@ -10,22 +10,40 @@ import Alamofire
 import NVActivityIndicatorView
 
 class DeleveryViewController: UIViewController {
-
-    var currentPage = 0
-    var isLoadedAllData = false
-    var saveData = [Any]()
-    
-    var writeButton: UIBarButtonItem!
-    var searchButton: UIBarButtonItem!
     
     @IBOutlet weak var mainTableView: UITableView!
-    
+    var writeButton: UIBarButtonItem!
+    var searchButton: UIBarButtonItem!
     var loading: NVActivityIndicatorView!
+
+    lazy var dataManager: DeleveryDataManager = DeleveryDataManager(view: self)
+    var deleveryPost: [Delevery] = []
+    var currentPage = 0
+    var isLoadedAllData = false
     
 //MARK: -생명주기
     override func loadView() {
         super.loadView()
+        setLoading()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        setTableView()
+        setNavigationBarItem()
+        dataManager.getAllPost(viewController: self, page: currentPage)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItemUse()
+    }
+}
+//MARK: -기본 UI 함수
+extension DeleveryViewController {
+   
+    func setLoading() {
         loading = NVActivityIndicatorView(frame: .zero, type: .ballBeat, color: UIColor.SBColor.SB_BaseYellow, padding: 0)
         loading.translatesAutoresizingMaskIntoConstraints = false
         
@@ -36,25 +54,8 @@ class DeleveryViewController: UIViewController {
             loading.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loading.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
-        
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        getDelivary(page: currentPage)
-        setTableView()
-        setNavigationBarItem()
-        
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        navigationItemUse()
-    }
-    
-//MARK: -기본 UI 함수
     
     func setNavigationBarItem() {
         self.navigationItem.title = "배달"
@@ -83,14 +84,18 @@ class DeleveryViewController: UIViewController {
         mainTableView.refreshControl = UIRefreshControl()
         mainTableView.refreshControl?.addTarget(self, action: #selector(refreshData), for: .valueChanged)
     }
+
+}
+
 //MARK: -스토리보드 Action 함수
+extension DeleveryViewController {
     @objc func refreshData() {
         print(">> 상단 새로고침")
         currentPage = 0
         self.isLoadedAllData = false
-        saveData.removeAll()
+        deleveryPost.removeAll()
         mainTableView.reloadData()
-        getDelivary(page: currentPage)
+        dataManager.getAllPost(viewController: self, page: currentPage)
         
     }
     
@@ -116,93 +121,30 @@ class DeleveryViewController: UIViewController {
         
         self.navigationController?.pushViewController(vc, animated: true)
     }
-    
- 
-//MARK: -API
-    func getDelivary(page: Int) {
-        loading.startAnimating()
-        
-        currentPage += 1
-        
-        guard isLoadedAllData == false
-        else {
-            loading.stopAnimating()
-            return
-        }
-        
-        let URL = "http://13.209.10.30:3000/home/delivery?page=\(currentPage)"
-        let alamo = AF.request(URL, method: .get, parameters: nil).validate(statusCode: 200...500)
-        
-        alamo.responseJSON { [self] (response) in
-            switch response.result {
-            case .success(let value):
-                if let jsonObj = value as? NSDictionary {
-                    print(">> \(URL)")
-                    print(">> 배달 게시글 API 호출 성공")
-                    
-                    mainTableView.refreshControl?.endRefreshing()
-                    loading.stopAnimating()
-                    let result = jsonObj.object(forKey: "check") as! Bool
-                    if result == true {
-                        let message = jsonObj.object(forKey: "message") as! String
-                        print(">> \(message)")
-                        let content = jsonObj.object(forKey: "content") as! NSArray
-                        
-                        guard content.count > 0 else {
-                            loading.stopAnimating()
-                            print(">> 더이상 읽어올 게시글 없음")
-                            print(">> 총 읽어온 게시글 개수 = \(saveData.count)")
-                            self.isLoadedAllData = true
-                            return
-                        }
-                        
-                        
-                        for i in 0..<content.count {
-                            saveData.append(content[i])
-                        }
-                        print(">> \(URL)")
-                        print(">> 읽어온 게시글의 개수: \(content.count), 현재 페이지\(page+1)")
-                        mainTableView.reloadData()
-                    } else {
-                        let message = jsonObj.object(forKey: "message") as! String
-                        let alert = UIAlertController(title: message, message: "", preferredStyle: .alert)
-                        let okButton = UIAlertAction(title: "확인", style: .default, handler: nil)
-                        alert.addAction(okButton)
-                        self.present(alert, animated: true, completion: nil)
-                    }
-                  
-                }
-            case .failure(let error):
-                if let jsonObj = error as? NSDictionary {
-                    print("서버통신 실패")
-                    print(jsonObj)
-                }
-            }
-        }
-    }
 }
 
+//MARK: -tableview 세팅
 extension DeleveryViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return saveData.count
+        return deleveryPost.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DeleveryTableViewCell", for: indexPath) as! DeleveryTableViewCell
         
-        if saveData.count != 0 {
-            let data = saveData[indexPath.row] as! NSDictionary
+        if deleveryPost.count != 0 {
+            let data = deleveryPost[indexPath.row]
             
-            cell.titleLabel.text = data["title"] as? String
-            cell.timeLabel.text = data["timeStamp"] as? String
-            cell.contentsLabel.text = data["text"] as? String
+            cell.titleLabel.text = data.title
+            cell.timeLabel.text = data.timeStamp
+            cell.contentsLabel.text = data.text
             
             var hashString = ""
             
-            let hashData = data["hash"] as! NSArray
+            let hashData = data.hash
             
             for i in 0..<hashData.count {
-                hashString += "#" + "\(hashData[i] as! String) "
+                hashString += "#" + "\(hashData[i]) "
             }
             
             cell.tagLabel.text = hashString
@@ -216,8 +158,8 @@ extension DeleveryViewController: UITableViewDelegate, UITableViewDataSource {
         
         cell.selectionStyle = .none
         
-        if indexPath.row == saveData.count - 1 {
-            getDelivary(page: currentPage)
+        if indexPath.row == deleveryPost.count - 1 {
+            dataManager.getAllPost(viewController: self, page: currentPage)
         }
         
         return cell
@@ -227,16 +169,16 @@ extension DeleveryViewController: UITableViewDelegate, UITableViewDataSource {
         let storyBoard = UIStoryboard(name: "In_Post", bundle: nil)
         let vc = storyBoard.instantiateViewController(withIdentifier: "DetailPostViewController") as! DetailPostViewController
         
-        let data = saveData[indexPath.row] as! NSDictionary
+        let data = deleveryPost[indexPath.row]
         
-        vc.getPostNumber = data["no"] as! Int
-        vc.getTitle = data["title"] as! String
-        vc.getCategory = data["category"] as! String
-        vc.getTime = data["timeStamp"] as! String
-        vc.getNickname = data["userNickname"] as! String
-        vc.getContents = data["text"] as! String
-        vc.getShowCount = data["viewCount"] as! Int
-        vc.getUserID = data["userId"] as! String
+        vc.getPostNumber = data.no
+        vc.getTitle = data.title
+        vc.getCategory = data.category
+        vc.getTime = data.timeStamp
+        vc.getNickname = data.userNickname
+        vc.getContents = data.text
+        vc.getShowCount = data.viewCount
+        vc.getUserID = data.userId
         
         //vc.delegate = self
         
@@ -255,12 +197,26 @@ extension DeleveryViewController: UITableViewDelegate, UITableViewDataSource {
 //    }
 }
 
+//MARK: -UpdateData 프로토콜 연결 함수
 extension DeleveryViewController: UpdateData {
     func update() {
         mainTableView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
         currentPage = 0
         isLoadedAllData = false
-        saveData = []
-        getDelivary(page: currentPage)
+        deleveryPost.removeAll()
+        dataManager.getAllPost(viewController: self, page: currentPage)
+    }
+}
+
+//MARK: -DataManager 연결 함수
+extension DeleveryViewController: DeleveryView {
+    func stopRefreshControl() {
+        self.mainTableView.refreshControl?.endRefreshing()
+    }
+    func startLoading() {
+        self.loading.startAnimating()
+    }
+    func stopLoading() {
+        self.loading.stopAnimating()
     }
 }
