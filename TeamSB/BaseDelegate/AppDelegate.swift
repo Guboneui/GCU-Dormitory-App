694eb6384394d016c20a9ef2baaa0b812018157f
Guboneui <starku2249@naver.com>
2021-08-23 01:32:44 +0900

    fcm foreground 오류 수정

--- TeamSB/BaseDelegate/AppDelegate.swift
+++ TeamSB/BaseDelegate/AppDelegate.swift
@@ -15,34 +15,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         Messaging.messaging().delegate = self
       
-//        if #available(iOS 10.0, *) {
-//          // For iOS 10 display notification (sent via APNS)
-//          UNUserNotificationCenter.current().delegate = self
-//
-//          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-//          UNUserNotificationCenter.current().requestAuthorization(
-//            options: authOptions,
-//            completionHandler: { didAllow, Error in
-//                print(didAllow)
-//                UserDefaults.standard.set(didAllow, forKey: "alertAccess")
-//            }
-//            
-//          )
-//        } else {
-//          let settings: UIUserNotificationSettings =
-//            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-//          application.registerUserNotificationSettings(settings)
-//        }
+        if #available(iOS 10.0, *) {
+          // For iOS 10 display notification (sent via APNS)
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { didAllow, Error in
+                print(didAllow)
+                UserDefaults.standard.set(didAllow, forKey: "alertAccess")
+            }
+            
+          )
+        } else {
+          let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
 
         application.registerForRemoteNotifications()
-
-        
         IQKeyboardManager.shared().isEnabled = true
         IQKeyboardManager.shared().isEnableAutoToolbar = false
         IQKeyboardManager.shared().shouldResignOnTouchOutside = true
-        
-     
-        
+
         return true
     }
     
@@ -86,6 +82,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
       completionHandler(UIBackgroundFetchResult.newData)
     }
+    
+   
 }
 
 
